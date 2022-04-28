c57c13b3f699999e781f0a9f8ec485fae232eef3
mahi1212 <mahi121.mr@gmail.com>
2022-04-28 16:44:45 +0600

    added new problems

--- 112A_Patya_&_String_Solution.cpp
+++ 112A_Patya_&_String_Solution.cpp
@@ -10,21 +10,12 @@ int main(){
     transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
     transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
 
-    for(int i= 1; i<=s1.length(); i++){
-        for(int j = 1; j<=s2.length(); j++){
-            if(s1[i]<s2[j]){
-                cout << -1 << endl;
-                break;
-            }if(s1[i]>s2[j]){
-                cout << 1 << endl;
-                break;
-            }if(s1[i]==s2[j]){
-                cout << 0 << endl;
-                break;
-            }
-        }
-        break;
+    if (s1 < s2) {
+        cout << -1;
+    } else if (s1 > s2) {
+        cout << 1;
+    } else if (s1 == s2) {
+        cout << 0;
     }
-
     return 0;
 }
