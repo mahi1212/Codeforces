b215d5c31e7721af8e52508f5d756a03ce59363b
mahi1212 <mahi121.mr@gmail.com>
2022-08-01 13:17:08 +0600

    added new solutions

---
+++ 1157 A. Reachable Numbers.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define pb                  push_back
+#define yes                 cout <<"YES" <<endl
+#define no                  cout << "NO" <<endl
+#define newline             cout <<endl
+#define sa(a,n)             sort(a,a+n)
+#define sv(v)               sort(v.begin(),v.end())
+#define sumVec(v)           accumulate(v.begin(), v.end(), 0)
+#define fillVec(v,value)    fill(v.begin(), v.end(), value)
+#define Bismillah()         ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define oky_bye             return 0
+
+int main(){
+    Bismillah();
+
+    int n;
+    cin >> n;
+    int cnt = 0;
+    while(n>9){
+        cnt++; n++;
+        while(n%10==0){
+            n=n/10;
+        }
+        cout << n << " ";
+    }
+    cnt+=9;
+    cout << cnt << endl;
+    oky_bye;
+}
---
+++ 1366 A. Shovels and Swords.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define pb                  push_back
+#define yes                 cout <<"YES" <<endl
+#define no                  cout << "NO" <<endl
+#define newline             cout <<endl
+#define sa(a,n)             sort(a,a+n)
+#define sv(v)               sort(v.begin(),v.end())
+#define sumVec(v)           accumulate(v.begin(), v.end(), 0)
+#define fillVec(v,value)    fill(v.begin(), v.end(), value)
+#define Bismillah()         ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define oky_bye             return 0
+
+int main(){
+    Bismillah();
+
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b;
+        cin >> a >> b;
+        if(2*a <= b){
+            cout << a << endl;
+        }else if(2*b <= a){
+            cout << b << endl;
+        }else{
+            cout << (a+b)/3 << endl;
+        }
+
+    }
+
+    oky_bye;
+}
